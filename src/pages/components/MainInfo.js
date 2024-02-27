00b5d6bcac2f8783f91c6f3ee9ff8af2908718a7
Jinsol Kim <jinsol032360@gmail.com>
2024-02-27 16:27:17 +0900

    feat : invitation for father in law

---
+++ src/pages/components/MainInfo.js
@@ -0,0 +1,16 @@
+export default function MainInfo() {
+    return (
+        <div className="main-info">
+            <div className="main-info-title">
+                <p>The Wedding Of</p>
+                <p>
+                    신유환 <span>(안드레아)</span>& 김진솔 <span>(레아)</span>
+                </p>
+            </div>
+            <div className="main-info-location">
+                <p>2024년 04월 06일 (토) 11시 </p>
+                <p>서울 중림동 약현성당 </p>
+            </div>
+        </div>
+    );
+}
